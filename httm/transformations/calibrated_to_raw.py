1ad28a4a61497a89171bf3311de1246fcc855d9c
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-11-11 11:46:04 -0700

    Improving clarity of documentation

--- httm/transformations/calibrated_to_raw.py
+++ httm/transformations/calibrated_to_raw.py
@@ -16,7 +16,14 @@ from ..data_structures.common import Slice
 def add_start_of_line_ringing_to_slice(start_of_line_ringing, image_slice):
     # type: (numpy.ndarray, Slice) -> Slice
     """
-    Add a fixed pattern to each row of a slice.
+    This transformation introduces to each row the *start of line ringing* pattern.
+
+    Every uncalibrated row of pixels (dark or otherwise) in a CCD slice
+    has been observed in the lab to start with a fixed pattern.
+
+    This pattern is the *start of line ringing*.
+
+    Start of line ringing is most prominent in the left dark pixel columns.
 
     :param start_of_line_ringing: One dimensional array of floats, representing a fixed pattern \
     disturbance in each row of a slice.
@@ -25,43 +32,52 @@ def add_start_of_line_ringing_to_slice(start_of_line_ringing, image_slice):
     :type image_slice: :py:class:`~httm.data_structures.common.Slice` units: electrons
     :rtype:  :py:class:`~httm.data_structures.common.Slice`
     """
+    assert image_slice.units == "electrons", "units must be electrons"
     # noinspection PyProtectedMember
-    return image_slice._replace(
-        pixels=image_slice.pixels + start_of_line_ringing)
+    return image_slice._replace(pixels=image_slice.pixels + start_of_line_ringing)
 
 
 def add_pattern_noise_to_slice(pattern_noise, image_slice):
     # type: (numpy.ndarray, Slice) -> Slice
     """
-    Add a fixed pattern to a slice.
+    This transformation adds a fixed pattern of noise to a slice.
 
-    :param pattern_noise: Two dimensional array of floats, representing a fixed pattern \
+    The fixed pattern noise is different for each slice, for each CCD.
+
+    The default values should be zero, because the expected pattern noise is zero.
+
+    This is here simply because this effect might be noticeable in the flight electronics.
+
+    :param pattern_noise: A two dimensional array of floats, representing a fixed pattern \
     disturbance in a slice.
     :type pattern_noise: :py:class:`numpy.ndarray`
-    :param image_slice: input slice. Units: electrons
+    :param image_slice: The input slice. Units: electrons
     :type image_slice: :py:class:`~httm.data_structures.common.Slice`
     :rtype:  :py:class:`~httm.data_structures.common.Slice`
     """
+    assert image_slice.units == "electrons", "units must be electrons"
     # noinspection PyProtectedMember
-    return image_slice._replace(
-        pixels=image_slice.pixels + pattern_noise)
+    return image_slice._replace(pixels=image_slice.pixels + pattern_noise)
 
 
 def introduce_smear_rows_to_slice(smear_ratio, image_slice):
     # type: (float, Slice) -> Slice
     """
-    Estimate smear by averaging rows in the image pixels and then multiplying by ``smear_ratio``.
-    For most of the frame cycle, the pixel effectively sits in the imaging area of the CCD, collecting
-    photons from a particular point on the sky for the exposure time. During readout, the pixel moves
+    This function takes a slice with empty smear rows and populates them, and adds smear to every image
+    (but not dark pixels). See :ref:`ccd-and-slice-layout` for more details.
+
+    *Smear* is estimated by averaging rows in the image pixels and then multiplying by ``smear_ratio``.
+    For most of the frame cycle, a pixel effectively sits in the imaging area of the CCD, collecting
+    photons from a particular point on the sky for the exposure time. During readout, that pixel moves
     quickly through the imaging area, exposed to each point on the sky along a column for a short time,
     the parallel clock period. The ``smear_ratio`` is the ratio of exposure time to parallel clock period.
 
     This function first adds up all of the rows in the image pixel subarray of the slice. It multiplies
-    by smear_ratio to estimate a smear row. It then replaces the smear rows in the slice with the estimated
-    smear row, and adds the estimated smear row to each image row.
+    that sum by ``smear_ratio`` to estimate a smear row. It then replaces the corresponding empty smear rows in the
+    slice with the estimated smear row. Finally, it adds the estimated smear row to each image row.
     
-    The pixels in the resulting rows should all be nonzero for reasonable input data,
-    but if this transformation has not been applied, we expect zeros in those rows.
+    The pixels in the resulting smear rows should all be nonzero for reasonable input data,
+    but if this transformation has not been applied, they should always contain zeros for a slice of calibrated data.
 
     :param image_slice: input slice. Units: electrons
     :type image_slice: :py:class:`~httm.data_structures.common.Slice`
@@ -69,7 +85,7 @@ def introduce_smear_rows_to_slice(smear_ratio, image_slice):
     :type smear_ratio: float
     :rtype: :py:class:`~httm.data_structures.common.Slice`
     """
-    # TODO crash if smear rows already introduced
+    assert image_slice.units == "electrons", "units must be electrons"
     assert False, "Smear rows are already introduced (should be set to 0)"
 
     # TODO relative coordinates
@@ -86,7 +102,8 @@ def introduce_smear_rows_to_slice(smear_ratio, image_slice):
 def add_shot_noise(image_slice):
     # type: (Slice) -> Slice
     """
-    Add `shot noise <https://en.wikipedia.org/wiki/Shot_noise>`_ to every pixel.
+    This transformation adds `shot noise <https://en.wikipedia.org/wiki/Shot_noise>`_ to every pixel.
+
     *Shot noise* is a fluctuation in electron counts.
 
     It is modeled as a Gaussian distributed error.
@@ -99,57 +116,64 @@ def add_shot_noise(image_slice):
     :type image_slice: :py:class:`~httm.data_structures.common.Slice`
     :rtype: :py:class:`~httm.data_structures.common.Slice`
     """
-    # TODO throw if units are not electrons
+    assert image_slice.units == "electrons", "units must be electrons"
     # noinspection PyProtectedMember
     return image_slice._replace(
         pixels=numpy.random.normal(loc=image_slice.pixels, scale=numpy.sqrt(image_slice.pixels)))
 
 
-def simulate_blooming_on_slice(full_well, blooming_threshold, nreads, image_slice):
+def simulate_blooming_on_slice(full_well, blooming_threshold, number_of_exposures, image_slice):
+    # type: (float, float, int, Slice) -> Slice
     """
-    Simulate `blooming <http://hamamatsu.magnet.fsu.edu/articles/ccdsatandblooming.html>`_ along columns
+    This function simulates `blooming <http://hamamatsu.magnet.fsu.edu/articles/ccdsatandblooming.html>`_ along columns
     in the image pixel array.
 
     Blooming is a diffusion process involving thermal excitation of electrons over the potential barriers
-    between pixels. The height of the potential barrier confining electrons to the pixel decreases with
-    increasing electron content in the pixel. The diffusion rate grows exponentially with decreasing barrier
+    between pixels. The height of the potential barrier confining electrons to the pixel decreases as
+    the electron count in the pixel increases. The diffusion rate grows exponentially with decreasing barrier
     height.
 
-    Here, we crudely model this process using two parameters. *blooming_threshold* is the number of electrons
-    in the pixel that suffices to drive significant diffusion. *full_well* is enough electrons to cause such
-    rapid diffusion that the pixel cannot hold more.
+    This process is modeled using three parameters:
 
-    The function *diffusion_step* performs one step in the diffusion process,
-    spreading charge above *blooming_threshold* to adjacent pixels along a column.
-    It assumes charge that diffuses beyond the limits of the row is lost.
+      - ``full_well``, the maximum number of electrons in a pixel. This is the number of electrons sufficient to cause
+         such rapid diffusion that the pixel cannot hold any more.
+      - ``blooming_threshold``, the number of electrons in the pixel that suffices to drive significant diffusion.
+      - ``number_of_exposures``, the number of stacked images in the slice.
 
-    The function *bloom_column* applies *diffusion_step* until all pixels are below *full_well*.
-    It applies *diffusion_step* at least once to simulate the modest diffusion that may happen even if
-    no pixel in the column exceeds *full_well*.
+    Blooming is modeled as a diffusion process.
 
-    :param image_slice: input slice. Units: electrons
-    :param full_well: maximum number of electrons in a pixel.
-    :param blooming_threshold: number of electrons in a pixel for noticeable blooming.
-    :param nreads: number of stacked images in the slice
+    In a single step of the diffusion process, those pixels with electrons above
+    :math:`\\mathtt{number\_of\_exposures} \\times \\mathtt{blooming\_threshold}` electrons diffuse charge to
+    neighboring pixels in the same column.
+
+    A single step of the diffusion process is always performed *at least once*.
+
+    The single step is repeated until all pixels are below
+    :math:`\\mathtt{number\_of\_exposures} \\times \\mathtt{full\_well}`.
+
+    :param full_well: The maximum number of electrons in a pixel.
+    :type full_well: float
+    :param blooming_threshold: The number of electrons in the pixel that suffices to drive significant diffusion.
+    :type blooming_threshold: float
+    :param number_of_exposures: The number of stacked images in the slice
+    :type number_of_exposures: int
+    :param image_slice: Input slice. Units: electrons
+    :type image_slice: :py:class:`~httm.data_structures.common.Slice`
     :rtype: :py:class:`~httm.data_structures.common.Slice`
     """
-    # TODO crash if smear rows already introduced
-
-    # TODO check units=electrons
-
-    # TODO relative coordinates
-
-    kernel = numpy.array([0.3, 0.4, 0.3])
+    assert image_slice.units == "electrons", "units must be electrons"
+    convolutional_kernel = numpy.array([0.3, 0.4, 0.3])
 
     def diffusion_step(column):
-        diffusion_proof_part = numpy.clip(column, 0, nreads * blooming_threshold)
+        diffusion_proof_part = numpy.clip(column, 0, number_of_exposures * blooming_threshold)
         excess = column - diffusion_proof_part
-        diffused_excess = numpy.convolve(excess, kernel, mode='same')   # implicitly lose charge from top and bottom
+        # implicitly lose charge from top and bottom
+        diffused_excess = numpy.convolve(excess, convolutional_kernel, mode='same')
         return diffusion_proof_part + diffused_excess
 
     def bloom_column(column):
         column = diffusion_step(column)
-        while numpy.amax(column) > nreads * full_well:
+        while numpy.amax(column) > number_of_exposures * full_well:
             column = diffusion_step(column)
         return column
 
@@ -161,32 +185,37 @@ def simulate_blooming_on_slice(full_well, blooming_threshold, nreads, image_slic
     return image_slice._replace(pixels=working_pixels)
 
 
-def add_readout_noise_to_slice(readout_noise, nreads, image_slice):
+def add_readout_noise_to_slice(readout_noise, number_of_exposures, image_slice):
     """
-    Adds a Gaussian random *readout* noise to every pixel.
+    This transformation a Gaussian random *readout* noise to every pixel.
+
     This noise comes from the charge sense transistor and the signal processing electronics.
+
     The average value :math:`\\mu` of the noise is `0`.
-    The variance :math:`\\sigma^2` is :math:`\\mathtt{readout\_noise}^2 \\times \\mathtt{nreads}`
+
+    The variance :math:`\\sigma^2` is :math:`\\mathtt{readout\_noise}^2 \\times \\mathtt{number\_of\_exposures}`.
 
     :param readout_noise: noise standard deviation for one image
     :type readout_noise: float
-    :param nreads: number of stacked images in the slice
-    :type nreads: int
+    :param number_of_exposures: number of stacked images in the slice
+    :type number_of_exposures: int
     :param image_slice: input slice
     :type image_slice: :py:class:`~httm.data_structures.common.Slice`
     :rtype: :py:class:`~httm.data_structures.common.Slice`
     """
-    from numpy.random import normal
+    assert image_slice.units == "electrons", "units must be electrons"
     # noinspection PyProtectedMember
     return image_slice._replace(
-        pixels=image_slice.pixels + normal(loc=0.0, scale=readout_noise * numpy.sqrt(nreads),
-                                           size=image_slice.pixels.size))
+        pixels=image_slice.pixels + numpy.random.normal(loc=0.0, scale=readout_noise * numpy.sqrt(number_of_exposures),
+                                                        size=image_slice.pixels.size))
 
 
 # noinspection PyProtectedMember
 def simulate_undershoot(undershoot, image_slice):
     """
-    When you have a bright pixel, the pixel to the right of it will appear dimmer.  This is _undershoot_.
+    When a CCD reads out a bright pixel, the pixel to the right of it appears artificially dimmer.
+
+    This is *undershoot*.
 
     This function simulates undershoot for a slice.
 
@@ -200,22 +229,28 @@ def simulate_undershoot(undershoot, image_slice):
     :type image_slice: :py:class:`~httm.data_structures.common.Slice`
     :rtype: :py:class:`~httm.data_structures.common.Slice`
     """
-    kernel = numpy.array([1.0, -undershoot])
+    assert image_slice.units == "electrons", "units must be electrons"
+    convolutional_kernel = numpy.array([1.0, -undershoot])
 
     def convolve_row(row):
-        return numpy.convolve(row, kernel, mode='same')
-	
+        return numpy.convolve(row, convolutional_kernel, mode='same')
+
     # noinspection PyProtectedMember
     return image_slice._replace(pixels=numpy.apply_along_axis(convolve_row, 1, image_slice.pixels))
 
 
-def convert_slice_electrons_to_adu(compression, number_of_exposures, video_scale, baseline_adu, clip_level_adu,image_slice):
+def convert_slice_electrons_to_adu(compression, number_of_exposures, video_scale, baseline_adu, clip_level_adu,
+                                   image_slice):
     # type: (float, int, float, float, int, Slice) -> Slice
     """
-    Simulate the nonlinear effects in the measurement of electrons.
+    This is the final transformation applied when converting an :py:class:`~httm.data_structures.common.Slice` from having
+    electrons as units to *Analogue to Digital Converter Units* (ADU).
+
+    This simulates the nonlinear effects in the measurement of electrons.
     
-    Other transformation functions handle linear and approximately linear effects. This
-    transformation is more complex and has more parameters because nonlinear effects are not
+    Other transformation functions handle linear and approximately linear effects.
+
+    This transformation is more complex and has more parameters because nonlinear effects are not
     so easily disentangled.
 
     :param compression: The relative decrease in video gain over the total ADC range
@@ -240,11 +275,12 @@ def convert_slice_electrons_to_adu(compression, number_of_exposures, video_scale
     exposure_baseline = baseline_adu * number_of_exposures
     exposure_clip_level = clip_level_adu * number_of_exposures
 
+    # noinspection PyTypeChecker
     def transform_electron_to_adu(electron):
-	from numpy import clip
-	return clip(
-		exposure_baseline + electron / (video_scale * (1.0 + compression_per_electron * electron)),
-	0, exposure_clip_level )
+        from numpy import clip
+        return clip(
+            exposure_baseline + electron / (video_scale * (1.0 + compression_per_electron * electron)),
+            0, exposure_clip_level)
 
     return Slice(index=image_slice.index,
                  units="ADU",
@@ -272,5 +308,6 @@ def convert_electrons_to_adu(calibrated_converter):
     # noinspection PyProtectedMember
     return calibrated_converter._replace(
         slices=tuple(
-            convert_slice_electrons_to_adu(compression, number_of_exposures, video_scale, baseline_adu, clip_level_adu, image_slice)
+            convert_slice_electrons_to_adu(compression, number_of_exposures, video_scale, baseline_adu, clip_level_adu,
+                                           image_slice)
             for (video_scale, image_slice) in zip(video_scales, image_slices)))
