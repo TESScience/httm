84a341c0281fbca7fdb1cdeae3a741e01272c9e5
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-11-11 10:13:11 -0700

    If an object does not exist, try loading it as a module instead before failing

--- test/scripts/python_object_exists.py
+++ test/scripts/python_object_exists.py
@@ -2,8 +2,8 @@
 from __future__ import print_function
 
 import importlib
-import sys
 import re
+import sys
 
 
 def check_if_python_object_exists(full_python_object_name):
@@ -15,7 +15,7 @@ def check_if_python_object_exists(full_python_object_name):
     :param full_python_object_name: A String containing the python docstring to look for
     """
     assert re.compile("^[a-zA-Z_][a-zA-Z0-9_.]*[a-zA-Z0-9_]").match(full_python_object_name) is not None, \
-        "Invalid python object name"
+        "Invalid python object name {}".format(full_python_object_name)
     python_object_part_names = full_python_object_name.split(".")  # type: tuple
     if len(python_object_part_names) == 1:
         importlib.import_module(python_object_part_names[0])
@@ -23,9 +23,12 @@ def check_if_python_object_exists(full_python_object_name):
         module_name = ".".join(python_object_part_names[:-1])  # type: str
         object_name = python_object_part_names[-1]  # type: str
         module = importlib.import_module(module_name)
-        assert hasattr(module, object_name), \
-            'Cannot find object "{object_name}" in module "{module_name}"'.format(object_name=object_name,
-                                                                                  module_name=module_name)
+        if not hasattr(module, object_name):
+            try:
+                importlib.import_module(module_name + "." + object_name)
+            except ImportError:
+                raise Exception('Cannot find object or module "{module_name}.{object_name}"'
+                                .format(object_name=object_name, module_name=module_name))
 
 
 if __name__ == "__main__":
