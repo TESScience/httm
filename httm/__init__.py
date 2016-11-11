5506e9e7b19e9b984731b36e8e445d2cf9f45705
John Doty <jpd@noqsi.com>
2016-11-11 11:58:12 -0700

    Stubs for FITS and HDU functions

--- httm/__init__.py
+++ httm/__init__.py
@@ -6,4 +6,26 @@ This module contains top level transformations for converting calibrated
 or raw TESS full frame FITS images between one another.
 """
 
-# TODO: Write convenience functions for converting an HDUList to an HDUList and a file to a file for both raw->calibrated and calibrated->raw
+# TODO: Write me
+def raw_hdulist_to_calibrated(hdulist):
+    pass
+
+# TODO: Write me
+def raw_fits_to_calibrated(fits_input_file, fits_output_file):
+    pass
+
+# TODO: Write me
+def calibrated_hdulist_to_raw(hdulist):
+    pass
+
+# TODO: Write me
+def calibrated_fits_to_raw(fits_input_file, fits_output_file):
+    pass
+
+# TODO: Write me
+def simulate_electronic_effects_on_hdulist(hdulist):
+    pass
+
+# TODO: Write me
+def simulate_electronic_effects_on_fits(fits_input_file, fits_output_file):
+    pass
