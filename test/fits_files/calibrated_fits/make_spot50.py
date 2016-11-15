83321cc0585d0c06df7f7f00b329dd10328a89c4
John Doty <jpd@noqsi.com>
2016-11-15 18:56:29 -0500

    First checkin of demo

--- test/fits_files/calibrated_fits/make_spot50.py
+++ test/fits_files/calibrated_fits/make_spot50.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 #
-# Make a 50 x 50 test image for blooming and undershoot
-# This puts 1000000 (implicitly electrons) in pixel [25, 25]. The rest of the image is zero.
+# Make a test image of four 50 x 50 slices for blooming and undershoot
+# This puts 1000000 (implicitly electrons) in pixel [25, 25]. The rest of the image is 100.
 import numpy
-image_data=numpy.zeros((50,50))
+image_data=numpy.zeros((50,200))+100
 image_data[25,25]=1000000
 from astropy.io import fits
 fits.writeto('spot50.fits', image_data, None, clobber=True)
