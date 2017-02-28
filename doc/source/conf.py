2efcdce484ee725a583596b1553b444b7f5e4a85
John Doty <jpd@noqsi.com>
2017-02-28 09:19:35 -0700

    Getting rid of matplotlib plotting in conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,7 +37,6 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'matplotlib.sphinxext.plot_directive',
 ]
 
 # Don't worry about nonlocal URIs
